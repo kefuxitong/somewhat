f99361fcfc9cf94dd03ca7a1fd16a5aaa99cd264
Your Name <you@example.com>
2018-02-10 16:03:43 +0800

    ddd

--- sh/controller.sh
+++ sh/controller.sh
@@ -4,18 +4,16 @@ echo $flag
 echo $rabbitmq
 echo $redisaddr
 echo $redispwd
+echo $bintype
 
+binPath=~/$bintype/
+binFile=~/$bintype/$bintype
 
-downloadurl=https://raw.githubusercontent.com/kefuxitong/somewhat/master/controller
-binPath=~/controller/
-binFile=~/controller/controller
-
-echo $downloadurl
 echo $binPath
 echo $binFile
 
 if [ "$flag" == "setup" ]; then 
-
+    
     echo "[安装]正在关闭进程和删除旧版bin文件..."
 
     ps -ef | grep $binFile | grep -v grep | awk '{print $2}' | xargs kill -9
@@ -23,19 +21,13 @@ if [ "$flag" == "setup" ]; then
     rm -rf $binPath
 
     mkdir $binPath
-
-
-    echo "[安装]正在下载bin文件..."
-    curl $downloadurl -o $binFile
-    chmod +x $binFile
-    echo "[安装]下载完成！"
-    ls $binPath
-    ls $binPath2
+    
+    cp /home/$bintype $binPath
 
     if [ -f $binFile ]; then
         cd $binPath
         nohup $binFile -rabbitmq=$rabbitmq -redisaddr=$redisaddr -redispwd=$redispwd > $binPath/logs.txt 2>&1 &
-        echo "[安装]启动成功！"
+         echo "[安装]启动成功！"
         echo "[安装]大吉大利，今晚吃鸡"
         exit
     fi
@@ -44,7 +36,6 @@ if [ "$flag" == "setup" ]; then
     exit
 
 fi
-
 
 
 if [ "$flag" == "delete" ]; then 
